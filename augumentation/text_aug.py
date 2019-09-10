78b78d040efe85c452a85e3478975084750eafab
buvata <taibk59@gmail.com>
2019-09-10 15:14:09 +0700

    refactor

--- augumentation/text_aug.py
+++ augumentation/text_aug.py
@@ -6,7 +6,7 @@ random.seed(1)
 
 word_sentiment = [ "thích", " không thích", " ko thích", "ko", "khong" ]
 stop_words = ['aaaa']
-vocab_synonyms = { 'yeu':['thich', 'ung', 'love']}
+vocab_synonyms = { 'yeu': [ 'thich', 'ung', 'love' ]}
 
 def get_only_chars(line):
 
@@ -33,39 +33,39 @@ def random_deletion(words, p):
     if len(words) == 1:
         return words
 
-	#randomly delete words 
+    # randomly delete words
     new_words = []
     for word in words :
         r = random.uniform(0, 1)
         if r > p or word in word_sentiment:
                 new_words.append(word)
 
-	#deleting all words, return a random word
+    # deleting all words, return a random word
     if len(new_words) == 0:
-	    rand_int = random.randint(0, len(words)-1)
-	    return [words[rand_int]]
+        rand_int = random.randint(0, len(words)-1)
+        return [words[rand_int]]
 
     return new_words
 
 
 def random_swap(words, n):
-	new_words = words.copy()
-	for _ in range(n):
-		new_words = swap_word(new_words)
-	return new_words
+    new_words = words.copy()
+    for _ in range(n):
+        new_words = swap_word(new_words)
+    return new_words
 
 
 def swap_word(new_words):
-	random_idx_1 = random.randint(0, len(new_words)-1)
-	random_idx_2 = random_idx_1
-	counter = 0
-	while random_idx_2 == random_idx_1:
-		random_idx_2 = random.randint(0, len(new_words)-1)
-		counter += 1
-		if counter > 3:
-			return new_words
-	new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1] 
-	return new_words
+    random_idx_1 = random.randint(0, len(new_words)-1)
+    random_idx_2 = random_idx_1
+    counter = 0
+    while random_idx_2 == random_idx_1:
+        random_idx_2 = random.randint(0, len(new_words)-1)
+        counter += 1
+        if counter > 3:
+            return new_words
+    new_words[random_idx_1], new_words[random_idx_2] = new_words[random_idx_2], new_words[random_idx_1]
+    return new_words
 
 
 def get_synonyms(word, vocab_synonyms):
@@ -114,10 +114,10 @@ def RD(sentence, p_rd, n_aug=3):
     augmented_sentences = []
 
     for _ in range(n_aug):
-	    a_words = random_deletion(words, p_rd)
-	    augmented_sentences.append(' '.join(a_words))
+        a_words = random_deletion(words, p_rd)
+        augmented_sentences.append(' '.join(a_words))
 
-	# augmented_sentences = [get_only_chars(sentence) for sentence in augmented_sentences]
+    # augmented_sentences = [get_only_chars(sentence) for sentence in augmented_sentences]
     shuffle(augmented_sentences)
 
     augmented_sentences.append(sentence)
@@ -126,23 +126,23 @@ def RD(sentence, p_rd, n_aug=3):
     
 
 def RS(sentence, p_rs, n_aug=3):
-	sentence = get_only_chars(sentence)
-	words = sentence.split(' ')
-	num_words = len(words)
+    sentence = get_only_chars(sentence)
+    words = sentence.split(' ')
+    num_words = len(words)
 
-	augmented_sentences = []
-	n_rs = max(1, int(p_rs*num_words))
+    augmented_sentences = []
+    n_rs = max(1, int(p_rs*num_words))
 
-	for _ in range(n_aug):
-		a_words = random_swap(words, n_rs)
-		augmented_sentences.append(' '.join(a_words))
+    for _ in range(n_aug):
+        a_words = random_swap(words, n_rs)
+        augmented_sentences.append(' '.join(a_words))
 
-	# augmented_sentences = [get_only_chars(sentence) for sentence in augmented_sentences]
-	shuffle(augmented_sentences)
+    # augmented_sentences = [get_only_chars(sentence) for sentence in augmented_sentences]
+    shuffle(augmented_sentences)
 
-	augmented_sentences.append(sentence)
+    augmented_sentences.append(sentence)
 
-	return augmented_sentences 
+    return augmented_sentences
 
 def SR(sentence, vocab_synonyms, p_sr, n_aug=2):
     sentence = get_only_chars(sentence)
